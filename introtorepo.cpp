276e2eda7ceb6e73030b310d26086c6af99f2413
rmasti <rlm7819@vt.edu>
2017-02-13 13:10:25 -0500

    again check authent

--- introtorepo.cpp
+++ introtorepo.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 int main(){
   double feval=360.0;
+
+  double pdflatexlatexbibtexlatex = 32.1;
   //Why does everything suck in the world
   cout << "Hello world" << endl;
   //better freaking work
